be0e7ef6d056d62c1502179c59d0c5f61dadfe44
Kim-Link <shkim@kristinkorea.com>
2022-12-15 10:42:24 +0900

    drf-jwt-swagger 완료

---
+++ drfjwt/drfjwt/urls.py
@@ -0,0 +1,79 @@
+"""drfjwt URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+
+
+from django.contrib import admin
+from django.urls import path, include, re_path as url
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+# 스웨거
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # 회원가입
+    path('api/accounts/v1/', include('accounts.urls')),
+
+]
+
+########### 스웨거 ############
+
+schema_url_patterns = [ 
+    path('api/accounts/v1/', include('accounts.urls')),
+    ]
+
+schema_view_v1 = get_schema_view(
+    openapi.Info(
+        title="drf-jwt-swagger API",
+        default_version='v1',
+        description="시스템 API",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    patterns=schema_url_patterns,
+)
+
+# Swagger
+if settings.DEBUG:
+    urlpatterns += [
+        # Auto DRF API docs
+        url(
+            r"^swagger(?P<format>\.json|\.yaml)/v1$",
+            schema_view_v1.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        url(
+            r"^swagger/v1/$",
+            schema_view_v1.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        url(
+            r"^redoc/v1/$",
+            schema_view_v1.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc-v1",
+        ),
+    ]
+
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 DRF로 로그인 기능 구현하기
+
+가상 환경 실행하기
+source ../VENV/v310Dj4drfjwt/bin/activate
+
+가상 환경 종료하기
+deactivate
